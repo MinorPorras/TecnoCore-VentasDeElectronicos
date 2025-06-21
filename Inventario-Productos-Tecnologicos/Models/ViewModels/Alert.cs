ae3cee573d3124ca9c02d62e5a98147e0cf4ce3a
MinorPorras <minorp1415@gmail.com>
2025-06-20 21:06:08 -0600

    Implementación del CRUD para cupones; se añadieron vistas para crear, editar y listar cupones, junto con validaciones y manejo de alertas.

---
+++ Inventario-Productos-Tecnologicos/Models/ViewModels/Alert.cs
@@ -0,0 +1,7 @@
+namespace Inventario_Productos_Tecnologicos.Models.ViewModels;
+
+public class Alert
+{
+    public string Type { get; set; } = "info";
+    public string Message { get; set; } = "";
+}
--- Inventario-Productos-Tecnologicos/webcomponent/DeleteDialogViewComponent.cs
+++ Inventario-Productos-Tecnologicos/webcomponent/DeleteDialogViewComponent.cs
@@ -6,8 +6,6 @@ public class DeleteDialogViewComponent : ViewComponent
 {
     public IViewComponentResult Invoke(string actionName, string controllerName)
     {
-        Console.WriteLine(actionName);
-        Console.WriteLine(controllerName);
         ViewBag.ActionName = actionName;
         ViewBag.ControllerName = controllerName;
         return View();
