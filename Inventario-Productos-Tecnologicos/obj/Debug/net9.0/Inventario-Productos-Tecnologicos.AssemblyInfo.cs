fe43f42c9689f81a01abd103b8f76db39c3c26ce
MinorPorras <minorp1415@gmail.com>
2025-07-03 00:18:48 -0600

    Estilos para las pestañas de index mantenimientos del sistema, bugfix busqueda clientes

--- Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
+++ Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e1938934a9dd3c519b9774d771d2db9bc7f69b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a646976ad8a7019182bf5ae91ff9ac897d184fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
