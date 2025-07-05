6cb028e632c78bd6416a0f89eb8fed426bab7066
MinorPorras <minorp1415@gmail.com>
2025-07-04 18:05:14 -0600

    Finalización estilado de los indexs

--- Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
+++ Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe43f42c9689f81a01abd103b8f76db39c3c26ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e32903eda70e35f3a2d62024fce9388198b716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
