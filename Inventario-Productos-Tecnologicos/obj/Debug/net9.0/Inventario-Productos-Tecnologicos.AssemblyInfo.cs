5cae2bdb1c79fb42ba3b0fe47f9fca8c18a49435
MinorPorras <minorp1415@gmail.com>
2025-07-15 14:34:56 -0600

    Estilos formularios Kardex

--- Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
+++ Inventario-Productos-Tecnologicos/obj/Debug/net9.0/Inventario-Productos-Tecnologicos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bf279409b562ca6e67799fdc9201c37ff95c82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44acefac248555e9eb82d325adb235ebf05a215")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario-Productos-Tecnologicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
